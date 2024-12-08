b9bb7969fee9eb7131bae45191aa9417f26fd994
hasithajayasundara <hasithajayasundara1@gmail.com>
2024-12-08 11:15:27 +0100

    day-8 part 2

--- day8.ts
+++ day8.ts
@@ -9,6 +9,8 @@ const isOnSameLine = (dX: number, dY: number, x1: number, y1: number, x: number,
   return (dX * (y - y1)) === (dY * (x - x1));
 };
 
+// part for point generation
+// deno-lint-ignore no-unused-vars
 const getPoint = (
   x: number,
   y: number,
@@ -42,7 +44,7 @@ const getPoint = (
       }
     }
   } else if (dir === 'LL') {
-    for (let i = x + 1; i <= ulX; i++) {
+    for (let i = x + 1; i < ulX; i++) {
       for (let j = y - 1; j >= 0; j--) {
         if (isOnSameLine(dX, dY, x, y, i, j)) {
           const d = distanceBetweenPoints(i, j, x, y);
@@ -68,11 +70,32 @@ const getPoint = (
   return null;
 };
 
+// part 2 point gen
+const getPoint2 = (
+  x: number,
+  y: number,
+  dX: number,
+  dY: number,
+  ulX: number,
+  ulY: number,
+) => {
+  const positions = [];
+  for (let i = 0; i < ulX; i++) {
+    for (let j = 0; j < ulY; j++) {
+      if (isOnSameLine(dX, dY, x, y, i, j)) {
+        positions.push([i, j]);
+      }
+    }
+  }
+
+  return positions;
+};
+
 
 try {
   const data = fs.readFileSync("day8.txt", "utf-8");
-  const grid = data.split("\n").filter(Boolean).map((line) => [...line])
-  //const directions = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]];
+  const grid = data.split("\n").filter(Boolean).map((line) => [...line]);
+  const copy = grid.map((l) => [...l]);
 
   const rows = grid.length;
   const columns = grid[0].length;
@@ -91,35 +114,36 @@ try {
     }
   }
 
-  console.log('rows', rows, "columns", columns, map);
-
-  const set = new Set();
-
   map.forEach((points) => {
     for (let i = 0; i < points.length; i++) {
       const p = points[i];
       for (let j = i + 1; j < points.length; j++) {
         const p1 = points[j];
-        const d = distanceBetweenPoints(p[0], p[1], p1[0], p1[1]);
         const dX = p[0] - p1[0];
         const dY = p[1] - p1[1];
-        const dirRes1 = dY > 0 ? 'UR' : 'UL';
-        const dirRes2 = dY > 0 ? 'LL' : 'LR';
-        const res1 = getPoint(p[0], p[1], dX, dY, dirRes1, rows, columns, d);
-        const res2 = getPoint(p1[0], p1[1], dX, dY, dirRes2, rows, columns, d);
-        if (res1) {
-          set.add(`${res1[0]} ${res1[1]}`);
-        }
-        if (res2) {
-          set.add(`${res2[0]} ${res2[1]}`);
+
+        //const dirRes1 = dY > 0 ? 'UR' : 'UL';
+        //const dirRes2 = dY > 0 ? 'LL' : 'LR';
+
+        const res1 = getPoint2(p[0], p[1], dX, dY, rows, columns);
+        const res2 = getPoint2(p1[0], p1[1], dX, dY, rows, columns);
+        res1.forEach(pn => {
+          copy[pn[0]][pn[1]] = "#";
+        });
+
+        res2.forEach(pn => {
+          copy[pn[0]][pn[1]] = "#";
+        });
+
+        if (res1.length || res2.length) {
+          copy[p[0]][p[1]] = "#";
+          copy[p1[0]][p1[1]] = "#";
         }
       }
     }
   });
 
-  console.log(set);
+  console.log(copy.reduce((acc, curr) => acc + curr.filter((i) => (i === "#")).length, 0));
 } catch (err) {
   console.error(err);
 }
-
-// 6 2
